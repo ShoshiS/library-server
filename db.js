2db2aa9e660785095560d9d99172d6bf97f31c07
shoshi safrai <sh053419431@gmail.com>
2025-11-02 23:29:59 +0200

    basic library server - lesson 5

---
+++ db.js
@@ -0,0 +1,43 @@
+const books = [
+    {
+        id: 1,
+        name: 'wantted',
+        category : 'thriller',
+        price : 80,
+        isBorrowed : false,
+        borrowArr : []
+    },
+    {
+        id: 2,
+        name: 'double agent',
+        category : 'thriller',
+        price : 90,
+        isBorrowed : false,
+        borrowArr : []
+    },
+    {
+        id: 3,
+        name: 'dina dei',
+        category :'kids',
+        price : 40,
+        isBorrowed : false,
+        borrowArr : []
+    },
+    {
+        id: 4,
+        name: 'titanic',
+        category : 'comics',
+        price : 30,
+        isBorrowed : false,
+        borrowArr : []
+    },
+    {
+        id: 5,
+        name: 'encyclopedia',
+        category : 'science',
+        price : 110,
+        isBorrowed : false,
+        borrowArr : []
+    },
+]
+export default books;
